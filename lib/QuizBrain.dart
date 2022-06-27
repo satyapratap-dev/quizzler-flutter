5571c268dc9f8c47e65c685258ae20ad59e82060
FlutLab.io <>
2022-06-27 03:05:22 +0000

    Autosave

--- lib/QuizBrain.dart
+++ lib/QuizBrain.dart
@@ -1,7 +1,7 @@
 import 'package:quizzler/question.dart';
 
 class QuizBrain {
-  List<Question> questionsBank = [
+  List<Question> _questionsBank = [
     Question('Some cats are actually allergic to humans', true),
     Question('You can lead a cow down stairs but not up stairs.', false),
     Question('Approximately one quarter of human bones are in the feet.', true),
@@ -16,4 +16,12 @@ class QuizBrain {
     Question('Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.', true),
     Question('In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.', true),
   ];
+
+  String getQuestionText(int questionNumber) {
+    return _questionsBank[questionNumber].questionText.toString();
+  }
+
+  bool? getQuestionAnswer(int questionNumber) {
+    return _questionsBank[questionNumber].questionAnswer;
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -56,7 +56,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                quesions.questionsBank[questionNumber].questionText.toString(),
+                quesions.getQuestionText(questionNumber),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -83,14 +83,12 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 //The user picked true.
                 setState(() {
-                  if (questionNumber < quesions.questionsBank.length) {
-                    if (quesions.questionsBank[questionNumber].questionAnswer == true)
-                      scoreKeeper.add(trueIcon());
-                    else
-                      scoreKeeper.add(falseIcon());
+                  if (quesions.getQuestionAnswer(questionNumber) == true)
+                    scoreKeeper.add(trueIcon());
+                  else
+                    scoreKeeper.add(falseIcon());
 
-                    questionNumber++;
-                  }
+                  questionNumber++;
                 });
               },
             ),
@@ -114,14 +112,12 @@ class _QuizPageState extends State<QuizPage> {
                 //The user picked false.
                 //The user picked true.
                 setState(() {
-                  if (questionNumber < quesions.questionsBank.length) {
-                    if (quesions.questionsBank[questionNumber].questionAnswer == false)
-                      scoreKeeper.add(trueIcon());
-                    else
-                      scoreKeeper.add(falseIcon());
+                  if (quesions.getQuestionAnswer(questionNumber) == false)
+                    scoreKeeper.add(trueIcon());
+                  else
+                    scoreKeeper.add(falseIcon());
 
-                    questionNumber++;
-                  }
+                  questionNumber++;
                 });
               },
             ),
